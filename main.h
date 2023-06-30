0abd118dcbb533b9be5998e89a8dfebbfaf4991b
Wahomethegeek <gatwirikelvin@outlook.com>
2023-06-30 23:45:32 +0300

    handling specifiers

--- main.h
+++ main.h
@@ -15,4 +15,5 @@ int print_escaped_string(char *str);
 int print_reversed_string(char *str);
 int print_rot13_string(char *str);
 int handle_format(char format, va_list args, int *sum);
+int handle_integers(va_list args, int *sum);
 #endif
